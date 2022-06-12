ae6a9bb4c6666279b7d3ea99ceecd248cd37afc8
Ted Janney <janneyt@oregonstate.edu>
2022-06-12 22:01:03 +1000

    Setup variables for box (imgur link from this)

--- generate.py
+++ generate.py
@@ -3,6 +3,16 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF ( "box.sdf" )
 
-pyrosim.Send_Cube ( name="Box", pos=[0, 0, 0.5], size=[1, 1, 1] )
+# Cube's physical dimensions
+length = 1
+width = 2
+height = 3
+
+# Position coordinates for cube
+x = 0
+y = 0
+z = height/2
+
+pyrosim.Send_Cube ( name="Box", pos=[x, y, z], size=[length, width, height] )
 
 pyrosim.End ()
